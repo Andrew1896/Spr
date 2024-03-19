0179205f00dbe0cbe3a7f528a581e443a45abc5a
Andrei Cataraga <andreicataraga18@gmail.com>
2024-03-19 23:58:19 +0100

    Interface JPA Repository

---
+++ src/main/java/com/zaurtregulov/spring/springboot/spring_data_jpa/service/EmployeeServiceImpl.java
@@ -0,0 +1,34 @@
+package com.zaurtregulov.spring.springboot.spring_data_jpa.service;
+
+import com.zaurtregulov.spring.springboot.spring_data_jpa.dao.EmployeeRepository;
+import com.zaurtregulov.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        employeeRepository.saveEmployee(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        return employeeDAO.getEmployee(id);
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        employeeDAO.deleteEmployee(id);
+    }
+}
